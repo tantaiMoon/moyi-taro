c32c569d3fdd2d07579efb2980b846d59f5d19c7
王鹏斌 <wangpengbin@ctv.org.cn>
2020-04-12 13:46:59 +0800

    feat: 修改配置

--- src/weapp/store/data.ts
+++ src/weapp/store/data.ts
@@ -6,22 +6,23 @@ class DataStore {
   @observable nowWeather = []
 
   @action
-  getNowWeatherData = () => {
+  getNowWeatherData = (param) => {
     let params = {
-      url: WEATHER_API('now'),
+      url: WEATHER_API(param.type),
       method: 'GET',
       payload: {
-        location: 'beijing',
+        location: param.location || 'beijing',
         key: '7e27e89bf8bf4023b0e50401646fd1fb'
       },
       type: 2
     }
 
-    fetch(params)
-      .then(res => {
-        console.log('log --------- res: ', res)
-      }).catch(err => {
-        console.log('log --------- : ', err)
+    const weather = fetch(params) || Promise.resolve()
+    weather.then(res => {
+      console.log('log --------- res: ', res)
+      this.nowWeather = res
+    }).catch(err => {
+      console.log('log --------- : ', err)
     })
   }
 }
